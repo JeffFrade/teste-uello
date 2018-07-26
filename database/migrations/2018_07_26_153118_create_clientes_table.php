c2ab6dfdc99d1a6497d216eb2ffa165a8a3cfdf3
Jefferson Frade <jefferrson.frade@gmail.com>
2018-07-26 15:39:47 -0300

    Created migrations

--- database/migrations/2018_07_26_153118_create_clientes_table.php
+++ database/migrations/2018_07_26_153118_create_clientes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateClientesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('cliente_id');
+            $table->string('nome');
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->dateTime('data_nascimento');
+            $table->string('cpf', 11);
             $table->timestamps();
             $table->softDeletes();
         });
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clientes');
     }
 }
