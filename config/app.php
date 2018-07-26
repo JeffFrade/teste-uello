5b6b0b5f69a2158d0c3122126fd27ed37ad83a69
Jefferson Frade <jefferrson.frade@gmail.com>
2018-07-26 16:21:19 -0300

    Created PaginateHelper

--- config/app.php
+++ config/app.php
@@ -215,6 +215,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        // Helpers
+        'PaginateHelper' => App\Helpers\PaginateHelper::class,
     ],
 
 ];
