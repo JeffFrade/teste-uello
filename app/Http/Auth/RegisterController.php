2ca7831b504d772a22fda82b291916c00eba8e56
Jefferson Frade <jefferrson.frade@gmail.com>
2018-07-26 16:04:01 -0300

    Refactor Controllers and make fixies

--- app/Http/Auth/RegisterController.php
+++ app/Http/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use App\Repositories\Models\User;
+use App\Http\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
